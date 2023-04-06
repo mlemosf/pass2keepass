46daaf0beee17d10c1c058fab812dbc89046c950
Matheus Lemos <matheuslemosf@protonmail.com>
2023-04-06 16:46:04 +0000

    Added console info to main file
    Information on the execution of the program.

--- src/main.py
+++ src/main.py
@@ -3,14 +3,36 @@ from utils.gpg import get_file_list, generate_password_list
 from utils.keepass import add_multiple_entries
 from dotenv import load_dotenv
 from os import getenv
+from colorama import Fore, Style
+import sys, traceback
 
 load_dotenv()
 
-PASSWORD_FOLDER = getenv("PASSWORD_FOLDER")
+def main():
 
-# Get all passwords and store in a list of dictionaries 
-folder_list = get_file_list(PASSWORD_FOLDER)
-password_list = generate_password_list(folder_list)
+    try:
+        PASSWORD_FOLDER = getenv("PASSWORD_FOLDER")
+        
+        # Get all passwords and store in a list of dictionaries 
+        print("==> Generating file list...", end=" ")
+        folder_list = get_file_list(PASSWORD_FOLDER)
+        print(Fore.GREEN + "OK" + Style.RESET_ALL)
 
-# Store all passwords in a Keepass kdbx group
-add_multiple_entries(getenv('KEEPASS_OUTPUT_FILE'), password_list)
+        print("==> Loading passwords...", end=" ")
+        password_list = generate_password_list(folder_list)
+        print(Fore.GREEN + "OK" + Style.RESET_ALL)
+        print(f"[INFO] Found {len(password_list)} passwords")
+        
+        # Store all passwords in a Keepass kdbx group
+        print("==> Storing passwords in kdbx file...", end=' ')
+        add_multiple_entries(getenv('KEEPASS_OUTPUT_FILE'), password_list)
+        print(Fore.GREEN + "OK" + Style.RESET_ALL)
+
+        # Fim da execução
+        print(Fore.GREEN + "DONE" + Style.RESET_ALL)
+    except Exception:
+        print(Fore.RED + "ERROR" + Style.RESET_ALL)
+        traceback.print_exc(file=sys.stdout)
+
+if __name__ == '__main__':
+    main()
